06b35db6daa1b6de9c0f9a60ebe57fe8863ee86a
airMay-hub <kiiikaaa.93@mail.ru>
2021-11-05 16:14:25 +0100

    feat: make 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(filePath, {encoding: 'utf-8'});
+
+stream.on('readable', () => {
+  let data = stream.read();
+  console.log(data);
+});
